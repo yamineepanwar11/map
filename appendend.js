db87c54f335f6e27065ce8bd9d29c0ed38b229d1
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-04-09 13:37:14 +0530

    first commit

---
+++ appendend.js
@@ -0,0 +1,11 @@
+let arr=["hey","hello","bye"];
+let a=arr.map((value)=>{
+    return value+"!"
+})
+console.log(a)
+
+// let arr=["Palak","Sakshi","Anu"];
+// let a=arr.map((value)=>{
+//     return value+" Panwar"
+// })
+// console.log(a)
